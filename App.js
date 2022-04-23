a536ff7412fa9ab3d37847f1ced782c4ea173eb4
Harsh Yadav <49532763+ashuharsh22@users.noreply.github.com>
2022-04-24 04:11:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar'
+import TextForm from './components/Textform'
+function App() {
+  return (
+    <>
+
+<Navbar title="TitleUtils" />
+<div className="container my-3">
+{<TextForm heading="Enter the text to analyze below"/>}
+{/*<About />*/}
+</div>
+</>
+);
+}
+
+export default App;
