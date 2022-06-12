53fce4d8104c373ac794dbef508e4c8e45bfab90
Harsh Yadav <49532763+ashuharsh22@users.noreply.github.com>
2022-06-12 13:08:44 +0530

    Star rating

--- src/App.js
+++ src/App.js
@@ -1,60 +1,60 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Textform from './components/Textform';
-import About from './components/About';
-import React, { useState } from 'react';
-import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
+import About from "./components/About";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import StarRating from "./components/Star";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
- 
 function App() {
-  const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
-      setAlert({
-        msg: message,
-        type: type
-      })
-      setTimeout(() => {
-          setAlert(null);
-      }, 1500);
-  }
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#042743';
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
     }
-  }
+  };
   return (
-  
-    <Router>
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
-    <Alert alert={alert}/>
-    <div className="container my-3">
-    <Switch>
+    <>
+      {/* <Router> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Switch>
+    <Route exact path="/"> */}
+        <Textform
+          showAlert={showAlert}
+          heading="Try TextUtils - word counter, character counter, remove extra spaces"
+          mode={mode}
+        />
+        <StarRating />
+        {/* </Route>
           <Route exact path="/about">
             <About mode={mode} />
-          </Route>
-          <Route exact path="/">
-            <Textform showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
-          </Route>
-    </Switch>
-    </div>
-    </Router>
-    
+          </Route> */}
+
+        {/* </Switch> */}
+      </div>
+      {/* </Router> */}
+    </>
   );
 }
 
